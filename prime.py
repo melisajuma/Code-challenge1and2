3a4625f1bbecf4aaef66028f831e8d2a264fd03d
melisajuma <Melisaakinyi95@gmail.com>
2019-12-10 16:06:35 +0300

    using modulus to gauge whethera number is prime or not and return boolean

--- prime.py
+++ prime.py
@@ -1,6 +1,13 @@
 def prime(num):
     if num > 1:
         for i in range(2,6):
-           
+            if (num % i) == 0: 
+                return False
+        else: 
+            return True
+    else: 
+        return False
+
+print(prime(121))
 
 
