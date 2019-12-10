896c0902df3bb543cb964218391ad1613f9ebe8c
melisajuma <Melisaakinyi95@gmail.com>
2019-12-10 16:08:59 +0300

    introducing the prime function

---
+++ memoize.py
@@ -0,0 +1,13 @@
+# from prime import prime
+
+
+def prime(num):
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0: 
+                return False
+        else: 
+            return True
+    else: 
+        return False
+
